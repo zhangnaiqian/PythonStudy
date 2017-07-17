beb4ac6bdd29808ea1fec797072483310f946ad5
Naiqian Zhang <zhangnaiqian1101@hotmail.com>
2017-07-17 12:58:41 -0400

    Learn Python the Hard Way SSS
    The book study

---
+++ PythonExercise.py
@@ -0,0 +1,12 @@
+sum = 0
+for x in range(101):
+	sum = sum + x
+print(sum)
+
+
+sum = 0
+n = 99
+while n > 0:
+	sum = sum + n
+	n = n - 2
+print(sum)
---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like tryping this."
+print "This is fun."
+print ""
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
